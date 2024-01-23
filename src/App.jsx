df972a4e18f19024c8fdcfc976581398857987bf
omar tarik yehya <khalaf.omar20@gmail.com>
2024-01-23 14:01:25 +0200

    feat: fetching meals data from backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
+import Meals from './components/Meals'
 
 const App = () => {
   return (
+    <>
     <Header/>
+    <Meals/>
+    </>
   )
 }
 
